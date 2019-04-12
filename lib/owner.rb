2f88f794cf438b521a696f2ccc6d03d8e5bad232
Drake Rentrop <drakerentrop@gmail.com>
2019-04-12 17:22:55 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,14 @@
+require "pry"
+
 class Owner
   # code goes here
   attr_reader :species
   attr_accessor :name, :pets, :fish, :dog, :cat
    @@all =[]
-   @@pets = {:fishes => [], :dogs => [], :cats => []}
+
 
    def initialize(species)
+     @pets = {:fishes => [], :dogs => [], :cats => []}
      @species = species
      @name = name
      @@all << self
@@ -28,35 +31,35 @@ class Owner
    end
 
    def pets
-     @@pets
+     @pets
    end
 
 def buy_fish(fish)
-  @@pets[:fishes] << Fish.new(fish)
+  @pets[:fishes] << Fish.new(fish)
 end
 
 def buy_dog(dog)
-  @@pets[:dogs] << Dog.new(dog)
+  @pets[:dogs] << Dog.new(dog)
 end
 
 def buy_cat(cat)
-  @@pets[:cats] << Cat.new(cat)
+  @pets[:cats] << Cat.new(cat)
 end
 
 def walk_dogs
-  @@pets[:dogs].each do |dog|
+  @pets[:dogs].each do |dog|
     dog.mood = "happy"
    end
  end
 
   def play_with_cats
-    @@pets[:cats].each do |cat|
+    @pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @@pets[:fishes].each do |fish|
+    @pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
@@ -66,12 +69,14 @@ def walk_dogs
   end
 
   def sell_pets
+    #binding.pry
   		pets.each do |species, animals|
   		 animals.each do |animal|
   		 	animal.mood = "nervous"
   		 end
   		 animals.clear
   		end
+      #binding.pry
   	end
 
 
